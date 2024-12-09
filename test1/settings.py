1a87346e03a9ab00281cea6f883ab05f816ddefa
Abishek Madhav <143243139+Abishek-Madhav@users.noreply.github.com>
2024-12-09 16:40:59 +0530

    Update settings.py

--- test1/settings.py
+++ test1/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'test1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Set to writable temp directory
     }
 }
 
